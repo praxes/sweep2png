7214f6245f82b6ec317c444da0ea9b8c6106245f
Darren Dale <dsdale24@gmail.com>
2014-12-17 16:55:27 -0500

    move stdint.h

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,4 +1,10 @@
-mv %RECIPE_DIR%\stdint.h %SRC_DIR%\include\
-cl /o sweep2png.exe src\sweep2png.cpp src\png.cpp /Iinclude /I%LIBRARY_INC% /Ilpng1615 lpng1615\libpng.lib zlib\zlib.lib
+CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64 /release
 
-mv sweep2png.exe %SCRIPTS%
+set LIBRARY_INC=C:\conda\Library\include
+set LIBRARY_LIB=C:\conda\Library\lib
+set Scripts=C:\conda\Scripts
+
+cl src\sweep2png.cpp src\png.cpp /Iinclude /I%LIBRARY_INC% %LIBRARY_LIB%\libpng.lib %LIBRARY_LIB%\zlib.lib
+
+if not exist "%SCRIPTS%" mkdir %SCRIPTS%
+cp sweep2png.exe %SCRIPTS%\sweep2png.exe
