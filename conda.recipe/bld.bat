4d6f3f644fcac7c40907af98ac53e01ae3658d76
Darren Dale <dsdale24@gmail.com>
2014-12-18 08:41:58 -0500

    adds test files

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,10 +1,7 @@
-CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64 /release
+CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64 /debug
+REM /release
 
-set LIBRARY_INC=C:\conda\Library\include
-set LIBRARY_LIB=C:\conda\Library\lib
-set Scripts=C:\conda\Scripts
-
-cl src\sweep2png.cpp src\png.cpp /Iinclude /I%LIBRARY_INC% %LIBRARY_LIB%\libpng.lib %LIBRARY_LIB%\zlib.lib
+cl src\sweep2png.cpp src\png.cpp /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng.lib %LIBRARY_LIB%\zlib.lib
 
 if not exist "%SCRIPTS%" mkdir %SCRIPTS%
 cp sweep2png.exe %SCRIPTS%\sweep2png.exe
