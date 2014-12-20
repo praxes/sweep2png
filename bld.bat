7f16335c7f58c2a02dea064ecded7bcced74da6c
Darren Dale <dsdale24@gmail.com>
2014-12-20 07:26:25 -0500

    windows working, if libpng.dll on PATH

--- bld.bat
+++ bld.bat
@@ -1,8 +1,14 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
 xcopy conda.recipe\stdint.h include\ /y
 
-REM edit below to find libpng and zlib
-cl /MD src\sweep2png.c src\png.c /Iinclude /IC:\conda\Library\include /link C:\conda\Library\lib\libpng.lib C:\conda\Library\lib\zlib.lib
+REM edit below to find libpng
+set LIBRARY_LIB=C:\conda\Library\lib
+set LIBRARY_INC=C:\conda\Library\include
+
+cl /MD src\sweep2png.c src\png.c /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng.lib
+mt.exe -manifest sweep2png.exe.manifest -outputresource:sweep2png.exe;1
+
+del sweep2png.exe.manifest
 
 REM run a quick test
 sweep2png.exe conda.recipe\Si_5grains_mesh.log conda.recipe/map/Si_70 0.7 cubic
--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,8 +1,10 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
-::dir %RECIPE_DIR%
+
 xcopy %RECIPE_DIR%\stdint.h include\ /y
 
 cl /MD src\sweep2png.c src\png.c /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng.lib %LIBRARY_LIB%\zlib.lib
+mt.exe -manifest sweep2png.exe.manifest -outputresource:sweep2png.exe;1
 
 if not exist "%SCRIPTS%" mkdir %SCRIPTS%
 xcopy sweep2png.exe %SCRIPTS%\
+::xcopy sweep2png.bat %SCRIPTS%\
