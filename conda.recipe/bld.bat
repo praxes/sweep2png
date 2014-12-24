0563cda92e47530f824fdb1e6ff18695e870bea3
Darren Dale <dsdale24@gmail.com>
2014-12-24 18:04:26 -0500

    working conda recipe for windows, static linking

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -2,9 +2,8 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
 
 xcopy %RECIPE_DIR%\stdint.h include\ /y
 
-cl /MD src\sweep2png.c src\png.c /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng.lib %LIBRARY_LIB%\zlib.lib
+cl /MD src\sweep2png.c src\png.c /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng_static.lib %LIBRARY_LIB%\zlibstatic.lib
 mt.exe -manifest sweep2png.exe.manifest -outputresource:sweep2png.exe;1
 
 if not exist "%SCRIPTS%" mkdir %SCRIPTS%
 xcopy sweep2png.exe %SCRIPTS%\
-::xcopy sweep2png.bat %SCRIPTS%\
