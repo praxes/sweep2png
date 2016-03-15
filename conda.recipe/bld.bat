46e2b6fc2652420bb7b1a48c793ae40640d62d95
Darren Dale <dsdale24@gmail.com>
2016-03-15 14:21:54 -0400

    cleanup, build with mingw on windows

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,11 +1,4 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
+gcc -static -g -O2 -Wall src/png.c src/sweep2png.c -Iinclude -I%LIBRARY_INC% -L%LIBRARY_LIB% -lpng -lz -o sweep2png
 
-xcopy %RECIPE_DIR%\stdint.h include\ /y
-
-cl /MD src\sweep2png.c src\png.c /Iinclude /I%LIBRARY_INC% /link %LIBRARY_LIB%\libpng_static.lib %LIBRARY_LIB%\zlibstatic.lib
-mt.exe -manifest sweep2png.exe.manifest -outputresource:sweep2png.exe;1
-
-::gcc -g -O2 -Wall src/png.c src/sweep2png.c -Iinclude -I%LIBRARY_INC% -L"%LIBRARY_LIB%" -llibpng -lzlib -o sweep2png.exe
-
-if not exist "%SCRIPTS%" mkdir %SCRIPTS%
-xcopy sweep2png.exe %SCRIPTS%\
+if not exist %SCRIPTS% mkdir -p %SCRIPTS%
+xcopy sweep2png.exe %SCRIPTS%
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,4 +1,4 @@
-export CFLAGS="-I$PREFIX/include -L$PREFIX/lib"
+gcc -g -O2 -Wall src/png.c src/sweep2png.c -Iinclude -I$PREFIX/include -L$PREFIX/lib -lpng -o sweep2png
 
-make
-make install
+mkdir -p $(PREFIX)/bin
+cp sweep2png $(PREFIX)/bin/sweep2png
